f53b19156f36d03c0be8d79a658ac31d70a9a95c
AhmedFathyDev <swe.ahmed.osman@gmail.com>
2024-09-17 00:25:41 +0300

    Add project files

--- Program.cs
+++ Program.cs
@@ -1,3 +1,30 @@
-﻿using System;
+﻿using Fathy.Swagger;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.OpenApi.Models;
+using Serilog;
 
-Console.WriteLine("Hello, World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration));
+
+var openApiInfo = new OpenApiInfo
+{
+    Title = "Fathy.Serilog",
+    Version = "v1"
+};
+
+builder.Services.AddSwaggerService(openApiInfo);
+
+var app = builder.Build();
+
+app.UseSerilogRequestLogging();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint($"/swagger/{openApiInfo.Version}/swagger.json", openApiInfo.Title);
+});
+
+app.MapGet("/", () => "Hello, World!");
+
+app.Run();
